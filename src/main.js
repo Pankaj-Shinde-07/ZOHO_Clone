e50b88e17299739da6496f809878f7fd8711ca21
Pankaj Shinde <shindepankaj428@gmail.com>
2026-06-22 09:26:03 +0530

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+  var currentPath = window.location.pathname.split("/").pop();
+  if (!currentPath || currentPath === "") {
+    currentPath = "index.html";
+  }
+
+  $(".nav-link").each(function () {
+    var href = $(this).attr("href") || "";
+    if (href.charAt(0) === "/") {
+      href = href.substring(1);
+    }
+    if (href === currentPath) {
+      $(this).addClass("text-danger").removeClass("text-dark");
+    }
+  });
+});
